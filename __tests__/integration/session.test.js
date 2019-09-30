f90c4f40f3b9c20d6dc8123505e7250bb1020a59
Mateus <mateus4k@protonmail.ch>
2019-09-30 12:52:33 -0300

    :rocket: Authentication

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -41,4 +41,48 @@ describe('Authentication', () => {
       })
     expect(response.status).toBe(401)
   })
+
+  it('should return jwt token when authenticated', async () => {
+    const user = await User.create({
+      name: 'Mateus',
+      email: 'mateus4k@protonmail.ch',
+      password: '123123'
+    })
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '123123'
+      })
+    expect(response.body).toHaveProperty('token')
+  })
+
+  it('should be able to access private routes when authenticated', async () => {
+    const user = await User.create({
+      name: 'Mateus',
+      email: 'mateus4k@protonmail.ch',
+      password: '123123'
+    })
+
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', `Bearer ${user.genereateToken()}`)
+
+    expect(response.status).toBe(200)
+  })
+
+  it('should not be able to access private routes when not authenticated', async () => {
+    const response = await request(app).get('/dashboard')
+
+    expect(response.status).toBe(401)
+  })
+
+  it('should not be able to access private routes when not authenticated', async () => {
+    const response = await request(app)
+      .get('/dashboard')
+      .set('Authorization', 'Bearer 123123')
+
+    expect(response.status).toBe(401)
+  })
 })
